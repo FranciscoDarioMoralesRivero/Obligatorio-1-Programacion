134fe97fbc7fcdeb6722c4aa19974f1fb3e3f09b
Nacho <126293348+IgnacioBadano@users.noreply.github.com>
2024-11-14 20:10:56 -0300

    Add files via upload

--- Obligatorio 1/clases.js/clases.js
+++ Obligatorio 1/clases.js/clases.js
@@ -1,52 +1,86 @@
-class Artista{
+class Artista {
 
-    constructor(Nombre,Edad,Caracteristicas){
-  
-        this.Nombre=Nombre;
-        this.Edad=Edad;
-        this.Caracteristicas=Caracteristicas;
+    constructor(nombre, edad, caracteristicas) {
+
+        this.nombre = nombre;
+        this.edad = edad;
+        this.caracteristicas = caracteristicas;
     }
-    
-    toString(){
+
+    toString() {
         return this.Nombre;
     }
-  }
+}
 
-  class Expocicion{
-  
-    constructor(Titulo,Fecha,Descripcion){
-  
-        this.Titulo=Titulo;
-        this.Fecha=Fecha;
-        this.Descripcion=Descripcion;
+class Exposicion {
+
+    constructor(titulo, fecha, descripcion) {
+
+        this.titulo = titulo;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
     }
-  }
-  class Comentario{
-  
-    constructor(Titulo,NombreDeVisita,Comentario,Calificacion,HiceVisitaGuiada){
-        this.Titulo=
-        this.NombreDeVisita=NombreDeVisita;
-        this.Comentario=Comentario;
-        this.Calificacion=Calificacion;
-        this.HiceVisitaGuiada=HiceVisitaGuiada;
+}
+
+class Comentario {
+
+    constructor(titulo, nombreDeVisita, comentario, calificacion, hiceVisitaGuiada) {
+        this.titulo = titulo;
+        this.nombreDeVisita = nombreDeVisita;
+        this.comentario = comentario;
+        this.calificacion = calificacion;
+        this.hiceVisitaGuiada = hiceVisitaGuiada;
     }
-  }
-  class Sistema {
+}
+
+class Sistema {
 
     constructor() {
         this.listaArtista = [];  // Inicializa una lista vacía para almacenar los artistas.
+        this.listaExposiciones = [];
+        this.listaComentarios = [];
     }
 
-    agregarArtista(Nombre,Edad,Caracteristicas) {
+    agregarArtista(nombre, edad, caracteristicas) {
         // Agrega un nuevo artista a la lista.
-        let objArtista = new Artista(Nombre,Edad,Caracteristicas)
+        let objArtista = new Artista(nombre, edad, caracteristicas)
         this.listaArtista.push(objArtista);
     }
 
     existeArtista(elNombre) {  // Verifica si un artista con el nombre dado ya existe en la lista.
         let existe = false;
         for (let e of this.listaArtista) {
-            if (e.Nombre === elNombre) {  // Compara el nombre del artista actual con el nombre buscado.
+            if (e.nombre === elNombre) {  // Compara el nombre del artista actual con el nombre buscado.
+                existe = true;
+            }
+        }
+        return existe;
+    }
+
+    agregarExposicion(titulo, fecha, descripcion) {
+        let objExposicion = new Exposicion(titulo, fecha, descripcion)
+        this.listaExposiciones.push(objExposicion);
+    }
+
+    existeExposicion(elTitulo) {
+        let existe = false;
+        for (let u of this.listaExposiciones) {
+            if (u.titulo === elTitulo) {
+                existe = true;
+            }
+        }
+        return existe;
+    }
+
+    agregarComentario(exposicion, nombreVisita, comentario, calificacion, hiceVisitaGuiada) {
+        let objComentario = new Comentario(exposicion, nombreVisita, comentario, calificacion, hiceVisitaGuiada) 
+        this.listaComentarios.push(objComentario);
+    }
+
+    existeComentario(elVisitante) {
+        let existe = false;
+        for (let o of this.listaComentarios) {
+            if (o.visitante === elVisitante) {
                 existe = true;
             }
         }
--- Obligatorio 1/funciones.js/funciones.js
+++ Obligatorio 1/funciones.js/funciones.js
@@ -1,57 +1,82 @@
-window.addEventListener("load",setup);
+window.addEventListener("load", setup);
 /*Comprobar funcionalidades*/
-let misistema= new Sistema();
+let misistema = new Sistema();
 
-function setup(){
-    document.getElementById("BotonColores").addEventListener("click",CambiarColor);
-    document.getElementById("BotonAltaArtista").addEventListener("click",AltaArtistas);
-    document.getElementById("BotonAltaExpocicion").addEventListener("click",AltaExpocicion);
-    document.getElementById("BotonAltaComentario").addEventListener("click",AltaComentario);
+function setup() {
+    document.getElementById("BotonColores").addEventListener("click", CambiarColor);
+    document.getElementById("BotonAltaArtista").addEventListener("click", AltaArtistas);
+    document.getElementById("BotonAltaExpocicion").addEventListener("click", AltaExposicion);
+    document.getElementById("BotonAltaComentario").addEventListener("click", AltaComentario);
 }
 
 function CambiarColor() {
-  let cambiarColor1 = document.getElementById("idCambioMuseo");
-  let cambiarColor2 = document.getElementById("idCambioIngresos");
-  let cambiarColor3 = document.getElementById("idCambioInfo");
+    let cambiarColor1 = document.getElementById("idCambioMuseo");
+    let cambiarColor2 = document.getElementById("idCambioIngresos");
+    let cambiarColor3 = document.getElementById("idCambioInfo");
 
-  let colorPrincipal = "rgb(152, 251, 152)";
-  let colorAlterno = "#A4D28C"; 
+    let colorPrincipal = "rgb(152, 251, 152)";
+    let colorAlterno = "#A4D28C";
 
-  let colorActual = window.getComputedStyle(cambiarColor1).backgroundColor;
+    let colorActual = window.getComputedStyle(cambiarColor1).backgroundColor;
 
-  if (colorActual === colorPrincipal) {
-      cambiarColor1.style.backgroundColor = colorAlterno;
-      cambiarColor2.style.backgroundColor = colorAlterno;
-      cambiarColor3.style.backgroundColor = colorAlterno;
-  } else {
-      cambiarColor1.style.backgroundColor = colorPrincipal;
-      cambiarColor2.style.backgroundColor = colorPrincipal;
-      cambiarColor3.style.backgroundColor = colorPrincipal;
-  }
+    if (colorActual === colorPrincipal) {
+        cambiarColor1.style.backgroundColor = colorAlterno;
+        cambiarColor2.style.backgroundColor = colorAlterno;
+        cambiarColor3.style.backgroundColor = colorAlterno;
+    } else {
+        cambiarColor1.style.backgroundColor = colorPrincipal;
+        cambiarColor2.style.backgroundColor = colorPrincipal;
+        cambiarColor3.style.backgroundColor = colorPrincipal;
+    }
 }
 
-function AltaArtistas(){
-    let form=document.getElementById("RegistrarArtistaForm");
-    if(form.reportValidity()){
-        let Nombre = document.getElementById("idNombre").value;
-        let Edad = document.getElementById("idEdad").value;
-        let Caracteristicas=document.getElementById("idEstilo").value;
-        if(misistema.existeArtista(Nombre)){
+function AltaArtistas() {
+    let form = document.getElementById("RegistrarArtistaForm");
+    if (form.reportValidity()) {
+        let nombre = document.getElementById("idNombre").value;
+        let edad = document.getElementById("idEdad").value;
+        let caracteristicas = document.getElementById("idEstilo").value;
+        if (misistema.existeArtista(nombre)) {
             alert("Ya Existe Este Artista")
-        }else{
-            misistema.agregarArtista(Nombre,Edad,Caracteristicas)
+        } else {
+            misistema.agregarArtista(nombre, edad, caracteristicas)
             alert("Alta Artistas")
-               // ActualizarListaArtista();
-                form.reset();
-            }
+            // ActualizarListaArtista();
+            form.reset();
         }
     }
-
-
-function AltaExpocicion(){
-    alert("Alta Expocicion")
 }
 
-function AltaComentario(){
-    alert("Alta Comentario")
+
+function AltaExposicion() {
+    let form = document.getElementById("IngresarExposicionesForm");
+    if (form.reportValidity()) {
+        let titulo = document.getElementById("idTitulo").value;
+        let comienzo = document.getElementById("idFecha").value;
+        let descripcion = document.getElementById("idDescripcion").value;
+        if (misistema.existeExposicion(titulo)) {
+            alert("Ya existe esta Exposicion")
+        } else {
+            misistema.agregarExposicion(titulo, comienzo, descripcion)
+            alert("Alta Exposiciones")
+            form.reset();
+        }
+    }
+}
+
+function AltaComentario() {
+    let form = document.getElementById("IngresarExposicionesForm");
+    if (form.reportValidity()) {
+        let exposicion = document.getElementById("idExposicion").value;
+        let visitante = document.getElementById("idVisitante").value;
+        let comentario = document.getElementById("idComentario").value;
+        let visita = document.getElementById("idVisita").value;
+        if (misistema.existeExposicion(visitante)) {
+            alert("Ya ingreso comentario este visitante")
+        } else {
+            misistema.agregarExposicion(exposicion, visitante, comentario, visita)
+            alert("Alta Comentarios")
+            form.reset();
+        }
+    }
 }
